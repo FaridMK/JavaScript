fe4a37136b920014d30306f3b26e8dbe63a7b0c3
FaridMK <neu.apply@gmail.com>
2022-09-20 23:15:08 +0400

    commit2

--- js/script.js
+++ js/script.js
@@ -2,23 +2,59 @@ const infoTab = [
 
     {
     name: 'console.log',
-    information: '... some information about this name and etc.',
-    syntaxis: 'console.log(x)',
+    information: '... используется для вывода данных в консол.',
+    syntaxis: 'console.log(message);',
 },
 {
     name: 'alert',
-    information: '... some information about this name and etc.',
-    syntaxis: 'alert(x)',
+    information: '... метод выводит модальное окно с сообщением, и две кнопки, Ok и Cancel.',
+    syntaxis: 'const message = "I am Ok!"; alert(message);',
 },
 {
     name: 'prompt',
-    information: '... some information about this name and etc.',
-    syntaxis: 'prompt(x)',
+    information: '... метод выводит модальное окно с полем ввода и кнопками: Ok и Cancel.',
+    syntaxis: 'const yourMessage = prompt("Please, type your message"); console.log(yourMessage);',
 },
 {
     name: 'confirm',
-    information: '... some information about this name and etc.',
-    syntaxis: 'cconfirm(x)',
+    information: '... метод выводит модальное окно с сообщением, и две кнопки, Ok и Cancel.',
+    syntaxis: 'const yourOrder = confirm("Please, confirm"); console.log(yourOrder);',
+},
+{
+    name: 'Number(val)',
+    information: '... функция используется для преобразования к числу.',
+    syntaxis: 'const valueA = "10"; console.log(Number(valueA));',
+    },
+{
+    name: 'Number.parseInt()',
+    information: '... Метод парсит из строки целое число.',
+    syntaxis: 'console.log(Number.parseInt("5px"));',
+    },
+
+{
+    name: 'Number.parseFloat()',
+    information: '... Метод Number.parseFloat() парсит из строки дробное число.',
+    syntaxis: 'console.log(Number.parseFloat("12.46qwe79"));',
+    },
+    {
+    name: 'toFixed()',
+    information: '... метод отсекает до определенного знака после запятой.',
+    syntaxis: 'console.log(0.1 + 0.32).toFixed(2));',
+},
+{
+    name: 'Math.floor(num)',
+    information: '... метод округляет в меньшую сторону.',
+    syntaxis: 'console.log(Math.floor(1.7)); // 1',
+},
+{
+    name: 'Math.ceil(num)',
+    information: '... метод округляет в большую сторону.',
+    syntaxis: 'console.log(Math.ceil(1.2)); // 2',
+},
+{
+    name: 'Math.round(num)',
+    information: '... метод округляет до ближайшего целого.',
+    syntaxis: 'console.log(Math.round(1.5)); // 2',
 },
 {
     name: 'NaN',
@@ -26,61 +62,25 @@ const infoTab = [
     syntaxis: 'NaN(x)',
     },
 {
-    name: 'rty',
-    information: '... rrrrrrrrtion about this name and etc.',
-    syntaxis: 'okkkkkk',
+    name: 'Math.max(num1, num2, ...)',
+    information: '...метод возвращает наибольшее число из набора.',
+    syntaxis: 'console.log(Math.max(20, 10, 50, 40)); // 50',
     },
 
 {
-    name: 'last',
-    information: '... some information about this name and etc.',
-    syntaxis: 'lasttttttttttt)',
+    name: 'Math.min(num1, num2, ...)',
+    information: '...метод возвращает наименьшее число из набора.',
+    syntaxis: 'console.log(Math.min(20, 10, 50, 40)); // 10',
     },
     {
-    name: 'console.log',
-    information: '... some information about this name and etc.',
-    syntaxis: 'console.log(x)',
+    name: 'Math.pow(base, exponent)',
+    information: '... метод возведения в степень.',
+    syntaxis: 'console.log(Math.pow(2, 4)); // 16',
 },
 {
-    name: 'alert',
-    information: '... some information about this name and etc.',
-    syntaxis: 'alert(x)',
-},
-{
-    name: 'prompt',
-    information: '... some information about this name and etc.',
-    syntaxis: 'prompt(x)',
-},
-{
-    name: 'confirm',
-    information: '... some information about this name and etc.',
-    syntaxis: 'cconfirm(x)',
-},
-{
-    name: 'NaN',
-    information: '... some information about this name and etc.',
-    syntaxis: 'NaN(x)',
-    },
-{
-    name: 'rty',
-    information: '... rrrrrrrrtion about this name and etc.',
-    syntaxis: 'okkkkkk',
-    },
-
-{
-    name: 'last',
-    information: '... some information about this name and etc.',
-    syntaxis: 'lasttttttttttt)',
-    },
-    {
-    name: 'console.log',
-    information: '... some information about this name and etc.',
-    syntaxis: 'console.log(x)',
-},
-{
-    name: 'alert',
-    information: '... some information about this name and etc.',
-    syntaxis: 'alert(x)',
+    name: 'Math.random()',
+    information: '... метод возвращает псевдослучайное число в диапазоне [0, 1).',
+    syntaxis: 'console.log(Math.random())',
 },
 {
     name: 'prompt',
@@ -1088,7 +1088,7 @@ for (const key of infoTab) {
     htmlLine +=
         "<li>"
         + "<span>" + key.name + "</span>" + "<br>" + "<br>"
-        + key.information + "<br>"
+        + key.information + "<br>" + "<br>"
         + key.syntaxis
         + "</li>";
 
